197b8926adc461eae71fbaa8aef598f76f8f31f9
Peter Kin <kinpeter85@gmail.com>
2022-05-17 16:45:07 +0200

    Add scripts

--- src/files.ts
+++ src/files.ts
@@ -1,10 +1,16 @@
 import * as path from 'path'
 import * as fs from 'fs'
-import { MPR_FOLDER_NAME } from './constants'
+import { DESKTOP_FILES_FOLDER, ICON_ASSET_FOLDER, MPR_FOLDER_NAME } from './constants'
 import Logger from './logger'
 import { GameProfile } from './types'
 import { getGameScript, mpr0Script, mpr1Script, mprGetScript } from './sh-profile-script'
-import { ValidateFunction } from 'ajv'
+import { gIcon } from './g-icon'
+import {
+  getGameDesktopFile,
+  getMprGetDesktopFile,
+  getProfile0DesktopFile,
+  getProfile1DesktopFile,
+} from './desktop-file'
 
 const logger = Logger.getInstance('Files')
 
@@ -60,6 +66,33 @@ export function createProfileScriptFiles(profiles: GameProfile[], homeDir: strin
 
 export function createDesktopFiles(profiles: GameProfile[], homeDir: string): void {
   try {
+    const desktopFolder = path.resolve(homeDir, DESKTOP_FILES_FOLDER)
+    if (!fs.existsSync(desktopFolder)) {
+      fs.mkdirSync(desktopFolder, { recursive: true })
+    }
+    const mprDesktopFile = path.resolve(desktopFolder, 'mpr.desktop')
+    const profile0DesktopFile = path.resolve(desktopFolder, 'mpr-0.desktop')
+    const profile1DesktopFile = path.resolve(desktopFolder, 'mpr-1.desktop')
+
+    if (fs.existsSync(mprDesktopFile)) fs.rmSync(mprDesktopFile)
+    fs.writeFileSync(mprDesktopFile, getMprGetDesktopFile(homeDir))
+
+    if (fs.existsSync(profile0DesktopFile)) fs.rmSync(profile0DesktopFile)
+    fs.writeFileSync(profile0DesktopFile, getProfile0DesktopFile(homeDir))
+
+    if (fs.existsSync(profile0DesktopFile)) fs.rmSync(profile1DesktopFile)
+    fs.writeFileSync(profile1DesktopFile, getProfile1DesktopFile(homeDir))
+
+    profiles.forEach(profile => {
+      if (profile.isDefault) return
+      const { abbreviation } = profile
+      const gameDesktopFile = path.resolve(desktopFolder, `mpr-${abbreviation}.desktop`)
+      const desktopFileContent = getGameDesktopFile(profile, homeDir)
+
+      if (fs.existsSync(gameDesktopFile)) fs.rmSync(gameDesktopFile)
+      fs.writeFileSync(gameDesktopFile, desktopFileContent)
+    })
+    logger.green(`Desktop files generated.`)
   } catch (e) {
     logger.red('Could not generate desktop files.')
     logger.def(e)
@@ -69,6 +102,17 @@ export function createDesktopFiles(profiles: GameProfile[], homeDir: string): vo
 
 export function copyIconFile(homeDir: string): void {
   try {
+    const iconFolder = path.resolve(homeDir, ICON_ASSET_FOLDER)
+    if (!fs.existsSync(iconFolder)) {
+      fs.mkdirSync(iconFolder, { recursive: true })
+    }
+    const iconFile = path.resolve(iconFolder, 'g.svg')
+    if (fs.existsSync(iconFile)) {
+      logger.cyan(`Icon file already exists at ${iconFile}`)
+      return
+    }
+    fs.writeFileSync(iconFile, gIcon)
+    logger.green(`Created icon file at ${iconFile}`)
   } catch (e) {
     logger.red('Could not generate icon file.')
     logger.def(e)
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { readConfigFile } from './config'
 import * as os from 'os'
 import { generateMainScript } from './sh-main-script'
-import { createMainScriptFile, createProfileScriptFiles, prepareFolder } from './files'
+import {
+  copyIconFile,
+  createDesktopFiles,
+  createMainScriptFile,
+  createProfileScriptFiles,
+  prepareFolder,
+} from './files'
 
 export function run() {
   const homeDir = os.homedir()
   const { profiles } = readConfigFile(homeDir)
   const mainScriptContent = generateMainScript(profiles)
   prepareFolder(homeDir)
+  copyIconFile(homeDir)
   createMainScriptFile(mainScriptContent, homeDir)
   createProfileScriptFiles(profiles, homeDir)
+  createDesktopFiles(profiles, homeDir)
 }
